4f484b24064e51ddab9708bb1092ff8222196d25
Vavarooks <vavarooks123@gmail.com>
2020-02-01 14:23:24 -0800

    Java edit, HTML edit

--- JavaScript/JavaPage.js
+++ JavaScript/JavaPage.js
@@ -1,43 +1,37 @@
-let mainEl = document.getElementById("main");
+// let mainEl = document.getElementById("main");
 
-let buttons = document.getElementById("buttons");
+// let buttons = document.getElementById("buttons");
 
-let day = 0
+// let day = 0
 
-day++;
+// day++;
 
-$(`#buttons`).on('click', function (event) {
-    event.preventDefault();
-    weekDay();
-  
-  });
+// $(`#buttons`).on('click', function (event) {
+//   event.preventDefault();
+//   weekDay();
 
-  function weekDay() {
-    for (let i = 0; i < planner.week.length; i++) {
-      $('#buttons').append(`<button class= "choose" data-choice= "${planner.week[i]}"
-  data-type='${planner.week[i]}'> ${planner.week[i]} </button>`);
-    };
+// });
 
-
-  };
-
-  $(`#buttons`).on('click', function (event) {
-    event.preventDefault();
-    month();
-  
-  });
-
-  function month() {
-    for (let i = 0; i < planner.month.length; i++) {
-      $('#buttons').append(`<button class= "choose" data-choice= "${planner.month[i]}"
-  data-type='${planner.month[i]}'> ${planner.month[i]} </button>`);
-    };
+// function weekDay() {
+//   for (let i = 0; i < planner[0].week.length; i++) {
+//     $('#buttons').append(`<button class= "choose" data-choice= "${planner[0].week[i]}"
+//   data-type='${planner[0].week[i]}'> ${planner[0].week[i]} </button>`);
+//   };
 
 
-  };
+//   $(`#buttons`).on('click', function (event) {
+//     event.preventDefault();
+//     month();
 
-  mainEl.append(`Day of the Week: ${planner.week[i]}`);
+//   });
 
-  mainEl.append(`Month: ${planner.month[i]}`);
+//   function month() {
+//     for (let i = 0; i < planner[1].month.length; i++) {
+//       $('#buttons').append(`<button class= "choose" data-choice= "${planner[1].month[i]}"
+//   data-type='${planner[1].month[i]}'> ${planner[1].month[i]} </button>`);
+//     };
 
- 
+//     $(`#main`).append(`Day of the Week: ${planner[0].week[i]}`);
+
+//     $(`#main`).append(`Month: ${planner[1].month[i]}`);
+
