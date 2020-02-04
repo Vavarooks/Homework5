919f2cfaf2e74f4f94f9aaf5284d7b24ab3fbf44
Vavarooks <vavarooks123@gmail.com>
2020-02-03 18:06:10 -0800

    Java, HTML, CSS edit

--- JavaScript/JavaPage.js
+++ JavaScript/JavaPage.js
@@ -10,38 +10,45 @@ let dayWeekMonth = 0;
 
 dayWeekMonth++;
 
-mainEl.append(planner[dayWeekMonth].week);
 
-$(`#buttons`).on('click', function (event) {
-    event.preventDefault();
-    plannerEl();
 
-});
 
-function plannerEl() {
-    $(`#main`).text(planner[dayWeekMonth].week);
-    $(`#buttons`).empty();
-    for (let i = 0; i < planner[dayWeekMonth].week; i++) {
-        $('#buttons').append(`<button class= "choose" data-choice= "${planner[dayWeekMonth].week}"'> ${planner[dayWeekMonth].week} </button>`)
 
-    };
-}
 
-$('#buttons').on('click', '.choose', function (event) {
-    event.preventDefault();
-    var userChoice = $(this).attr('data-choice');
 
-    alert(userChoice);
-    console.log(userChoice);
 
-    if (questionNum < questions.length - 1) {
-        questionNum++;
-        disQuestion();
-    }
-    else {
+// weekEl.append(planner[dayWeekMonth].week);
 
-        result();
-    }
+// $(`#buttons`).on('click', function (event) {
+//     event.preventDefault();
+//     plannerEl();
 
-})
+// });
+
+// function plannerEl() {
+//     $(`#main`).text(planner[dayWeekMonth].week);
+//     $(`#buttons`).empty();
+//     for (let i = 0; i < planner[dayWeekMonth].week; i++) {
+//         $('#buttons').append(`<button class= "choose" data-choice= "${planner[dayWeekMonth].week}"'> ${planner[dayWeekMonth].week} </button>`)
+
+//     };
+// }
+
+// $('#buttons').on('click', '.choose', function (event) {
+//     event.preventDefault();
+//     var userChoice = $(this).attr('data-choice');
+
+//     alert(userChoice);
+//     console.log(userChoice);
+
+//     if (questionNum < questions.length - 1) {
+//         questionNum++;
+//         disQuestion();
+//     }
+//     else {
+
+//         result();
+//     }
+
+// })
 
