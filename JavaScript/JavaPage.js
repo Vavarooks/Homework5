f1dced8e156060222d59e10478ed23e36b2c2994
Vavarooks <vavarooks123@gmail.com>
2020-02-03 18:24:53 -0800

    Java edit, README edit

--- JavaScript/JavaPage.js
+++ JavaScript/JavaPage.js
@@ -1,2 +1,24 @@
+let calander = document.getElementsByClassName(`container`)
 
+moment().format('MMMM Do YYYY, h:mm:ss a');
+
+moment().format('dddd');   
+
+moment().format("MMM Do YY");
+
+m
+  .format()
+  .format('dddd')
+  .format('MMM Do YY') // → "Sep 2nd 07"
+  .fromNow() // → "31 minutes ago"
+  .calendar() // → "Last Friday at 9:32PM"
+
+m.add(1, 'day')
+m.subtract(2, 'days')
+m.startOf('day')
+m.endOf('day')
+m.startOf('hour')
+
+.format('LLL')
+                                                     
 
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Homework5
+
+    - This homework can be accssed here:
+
+
+
+
+    - The homework contains three folders, a CSS folder, a HTML folder and a Javascript folder.
+
+    - The javascript holds the main backbone cose that makes the calander run.
+
+    - The CSS holds touch ups to the HTML page.
+
+    - THe HTML holds the bare bones of the page.
