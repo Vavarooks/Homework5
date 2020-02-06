14d5d9e24920ab1c98cf7b3eb8654d568ef828b9
Vavarooks <vavarooks123@gmail.com>
2020-02-05 20:19:39 -0800

    HTML java and css edits

--- JavaScript/JavaPage.js
+++ JavaScript/JavaPage.js
@@ -10,68 +10,9 @@ mainEl.append(planner);
 
 choiceEl.append(planner[plannerNum].button);
 
-plannerNUm++;
-
-$(`#answerChoice`).on('click', function (event) {
-  event.preventDefault();
-  $('#main').hide();
-  $('#answerChoice').hide();
-
-  weekDay();
-  console.log(setTime());
-
-});
-
-function weekDay() {
-  $(`#quest`).text(planner[plannerNum].week);
-  $(`#buttons`).empty();
-  for (let i = 0; i < planner[plannerNum].week.length; i++) {
-    $('#buttons').append(`<button class= "choose" data-choice= "${planner[plannerNum].week[i]}"
-data-type='${planner[plannerNum].week}'> ${planner[plannerNum].week[i]} </button>`)
+// Work on CSS & Java
+// Make it look like image
+// figure out how to get data to local storage
+// firgure out how to recive that data and put it to the div it was taken from
 
 
-  };
-}
-
-$('#buttons').on('click', '.choose', function (event) {
-  event.preventDefault();
-  var userChoice = $(this).attr('data-choice');
-
-  alert(userChoice);
-  console.log(userChoice);
-
-  if (plannerNum < planner.length - 1) {
-    plannerNum++;
-    disQuestion();
-  }
-  else {
-
-    result();
-  }
-
-})
-
-function result() {
-  console.log(wrongAnswer);
-  console.log(rightAnswer);
-  $(`#buttons`).empty();
-  
-}
-
-$(`.select`).click(function () {
-
-  let event = $(`#buttons`).val();
-
-  const scorer = {
-    event,
-    day,
-    month,
-  }
-
-  localStorage.setItem(`data`, JSON.stringify(scorer));
-
-});
-
-$(`.highScore`).text(`High-Score: ${initialsEl.initials} ${initialsEl.right} correct, and ${initialsEl.wrong} inncorrect with ${initialsEl.timeLeft} seconds left.`);
-
-console.log(initialsEl.initials)
